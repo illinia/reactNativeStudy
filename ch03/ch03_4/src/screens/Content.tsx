13e28d641f162525bab98ae741dd68f60e2c9607
illinia <67290425+illinia@users.noreply.github.com>
2021-06-22 20:52:33 +0300

    129~209

---
+++ ch03/ch03_4/src/screens/Content.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet, View, Image, ScrollView} from 'react-native';
+import * as D from '../data';
+
+const avatars = D.makeArray(200).map(_ => D.randomAvatarUrl());
+export default function Content() {
+  const children = avatars.map((avatarUrl, index) => (
+    <View key={index.toString()} style={styles.avatarView}>
+      <Image style={styles.avatar} source={{uri: avatarUrl}} />
+    </View>
+  ));
+  return (
+    <ScrollView contentContainerStyle={[styles.view]}>{children}</ScrollView>
+  );
+}
+// prettier-ignore
+const styles = StyleSheet.create({
+  view: {
+    flexDirection: 'row',
+    overflow: 'hidden',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    padding: 5,
+  },
+  text: {fontSize: 20},
+  avatarView: {padding: 3},
+  avatar: {width: 50, height: 50, borderRadius: 25},
+});
